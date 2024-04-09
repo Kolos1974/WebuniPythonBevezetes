c3653a28fea87f270df7dc788b76b9953fed7d2d
Kolos <karlovitz.kolos@gmail.com>
2024-04-09 12:46:44 +0200

    refactor

--- fibonacci_rf.py
+++ fibonacci_rf.py
@@ -18,7 +18,24 @@ def fibonacci(n):
             eredmény = fibonacci_rekurzió(x-1) + fibonacci_rekurzió(x-2)
         fibonacci_szám[x] = eredmény
         return eredmény
-    return fibonacci_rekurzió(n)
 
-print(fibonacci(6))  #8
 
+    def fibonacci_rekurziv(x):
+        if fibonacci_szám[x] is None:
+            if x <= 1:
+                fibonacci_szám[x] = x
+            else:
+                fibonacci_szám[x] = fibonacci_rekurziv(x-1) + fibonacci_rekurziv(x-2)
+        return fibonacci_szám[x]
+
+    # return fibonacci_rekurzió(n)
+
+    return fibonacci_rekurziv(n)
+
+
+print(fibonacci(6))   #8
+print(fibonacci(35))  #9227465
+
+for i in range(50):
+    print(i, fibonacci(i))
+    
